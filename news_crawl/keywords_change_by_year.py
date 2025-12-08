e1c4164916d40483f46c5f5b1c9cc82d0e14a1e1
Sun7257 <houng7257@gmail.com>
2025-12-08 19:49:53 +0900

    꺾은선 그래프도 가능하게 추가

--- news_crawl/keywords_change_by_year.py
+++ news_crawl/keywords_change_by_year.py
@@ -17,15 +17,19 @@ def keywords_change_by_year(df:pd):
 
     plt.rcParams['axes.unicode_minus'] = False
 
-    ax = grow_rate.plot(kind='bar', figsize=(12,6), width=0.7)
+    grow_rate_T = grow_rate.T
+    
+    #  꺾은선 그래프
+    # ax = grow_rate_T.plot(kind='bar', figsize=(12,6), width=0.7, colormap='Set2') 막대그래프
+    ax = grow_rate_T.plot(kind='line',marker='o', figsize=(12,6), colormap='Set2')
 
     plt.title('연도별 각 항목의 변화율', fontsize=15)
     plt.ylabel('변화율 (%)')
     plt.xticks(rotation=0)
-    plt.legend(title='연도', loc='upper right')
+    plt.legend(title='', loc='right', bbox_to_anchor=(1.1, 0.5))
 
     plt.tight_layout()
-    plt.savefig(save_dir_file)
+    plt.savefig(save_dir_file, bbox_inches='tight')
 
 if __name__ == "__main__":
     # 테스트용 데이터 입니다 
@@ -35,9 +39,9 @@ if __name__ == "__main__":
         '2022': [7000, 11000, 3700, 3000, 8000, 4000],
         '2023': [9000, 10000, 4000, 4000, 10000, 6000]
             }
-    index_labels = ['정책', '교육', '농사', '스마트',  '인구', '주택']
+    index_labels = ['정책', '교육', '농사', '스마트',  '인구', '토지']
     df = pd.DataFrame(data, index=index_labels) 
 
-    df1 = pd.read_csv("news_crawl/keywords_change_by_year/keywords_change_by_year.csv")
-    keywords_change_by_year(df)
+    df1 = pd.read_csv("news_crawl/keywords_change_by_year/keywords_change_by_year.csv" , index_col=0)
+    keywords_change_by_year(df1)
 
